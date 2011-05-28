51b3eb6e567eb066d95b5e2ecb6742cca2045d38
ikikko <ikikko+github@gmail.com>
2011-05-29 02:11:20 +0900

    add method getting project value into Backlog property

--- src/main/java/hudson/plugins/backlog/BacklogProjectProperty.java
+++ src/main/java/hudson/plugins/backlog/BacklogProjectProperty.java
@@ -1,55 +1,68 @@
 package hudson.plugins.backlog;
 
 import hudson.Extension;
-import hudson.model.AbstractProject;
 import hudson.model.Action;
-import hudson.model.Job;
 import hudson.model.JobProperty;
 import hudson.model.JobPropertyDescriptor;
+import hudson.model.AbstractProject;
+import hudson.model.Job;
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
- * Property for {@link AbstractProject} that stores the associated Backlog website URL.
- *
+ * Property for {@link AbstractProject} that stores the associated Backlog
+ * website URL.
+ * 
  * @see http://d.hatena.ne.jp/cactusman/20090328/p1
  * @author dragon3
  */
-public final class BacklogProjectProperty extends JobProperty<AbstractProject<?, ?>> {
+public final class BacklogProjectProperty extends
+		JobProperty<AbstractProject<?, ?>> {
 
-    public final String projectURL;
+	public final String projectURL;
 	public final String spaceURL;
 
 	@DataBoundConstructor
 	public BacklogProjectProperty(final String spaceURL) {
-        String tempS = null;
-        String tempP = null;
+		String tempS = null;
+		String tempP = null;
+
 		// normalize
 		if (spaceURL != null && spaceURL.length() > 0) {
-            if (spaceURL.indexOf("/projects/") > -1) {
-                tempS = spaceURL.substring(0, spaceURL.indexOf("/projects/") + 1);
-                tempP = spaceURL.substring(0, spaceURL.endsWith("/") ? spaceURL.length() - 1 : spaceURL.length());
-            }
-            else if (!spaceURL.endsWith("/")) {
-                tempS = spaceURL + '/';
-            }
-            else {
-                tempS = spaceURL;
-            }
-        }
+			if (spaceURL.indexOf("/projects/") > -1) {
+				tempS = spaceURL.substring(0,
+						spaceURL.indexOf("/projects/") + 1);
+				tempP = spaceURL.substring(0,
+						spaceURL.endsWith("/") ? spaceURL.length() - 1
+								: spaceURL.length());
+			} else if (!spaceURL.endsWith("/")) {
+				tempS = spaceURL + '/';
+			} else {
+				tempS = spaceURL;
+			}
+		}
 		this.spaceURL = tempS;
-        this.projectURL = tempP;
+		this.projectURL = tempP;
 	}
 
-    @Override
-    public Action getJobAction(AbstractProject<?,?> job) {
-        return new BacklogLinkAction(this);
-    }
+	public String getProject() {
+		if (projectURL == null) {
+			return null;
+		}
 
-    @Extension
-    public static final class DescriptorImpl extends JobPropertyDescriptor {
+		return projectURL.substring(projectURL.indexOf("/projects/")
+				+ "/projects/".length());
+	}
+
+	@Override
+	public Action getJobAction(AbstractProject<?, ?> job) {
+		return new BacklogLinkAction(this);
+	}
+
+	@Extension
+	public static final class DescriptorImpl extends JobPropertyDescriptor {
 
 		public DescriptorImpl() {
 			super(BacklogProjectProperty.class);
@@ -67,11 +80,13 @@ public final class BacklogProjectProperty extends JobProperty<AbstractProject<?,
 		}
 
 		@Override
-		public JobProperty<?> newInstance(StaplerRequest req, JSONObject formData) throws FormException {
-			BacklogProjectProperty bpp = req.bindJSON(BacklogProjectProperty.class, formData);
+		public JobProperty<?> newInstance(StaplerRequest req,
+				JSONObject formData) throws FormException {
+			BacklogProjectProperty bpp = req.bindJSON(
+					BacklogProjectProperty.class, formData);
 			if (bpp.spaceURL == null)
 				bpp = null; // not configured
 			return bpp;
 		}
-    }
+	}
 }
--- src/test/java/hudson/plugins/backlog/BacklogProjectPropertyTest.java
+++ src/test/java/hudson/plugins/backlog/BacklogProjectPropertyTest.java
@@ -1,51 +1,56 @@
 package hudson.plugins.backlog;
 
-import java.util.regex.Matcher;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import hudson.MarkupText;
-
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class BacklogProjectPropertyTest {
 
-    private BacklogProjectProperty bpp;
+	private BacklogProjectProperty bpp;
 
 	@Test
 	public final void testSpaceURLIsNull() {
-        bpp = new BacklogProjectProperty(null);
-        assertNull(bpp.spaceURL);
-        assertNull(bpp.projectURL);
+		bpp = new BacklogProjectProperty(null);
+		assertNull(bpp.spaceURL);
+		assertNull(bpp.projectURL);
+		assertNull(bpp.getProject());
 	}
 
 	@Test
 	public final void testSpaceURLIsProjectURL() {
-        bpp = new BacklogProjectProperty("https://demo.backlog.jp/projects/DORA");
-        assertNotNull(bpp.spaceURL);
-        assertNotNull(bpp.projectURL);
-        assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
-        assertEquals("https://demo.backlog.jp/projects/DORA", bpp.projectURL);
+		bpp = new BacklogProjectProperty(
+				"https://demo.backlog.jp/projects/DORA");
+		assertNotNull(bpp.spaceURL);
+		assertNotNull(bpp.projectURL);
+		assertNotNull(bpp.getProject());
+		assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
+		assertEquals("https://demo.backlog.jp/projects/DORA", bpp.projectURL);
+		assertEquals("DORA", bpp.getProject());
 
-        bpp = new BacklogProjectProperty("https://demo.backlog.jp/projects/FOO/");
-        assertNotNull(bpp.spaceURL);
-        assertNotNull(bpp.projectURL);
-        assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
-        assertEquals("https://demo.backlog.jp/projects/FOO", bpp.projectURL);
+		bpp = new BacklogProjectProperty(
+				"https://demo.backlog.jp/projects/FOO/");
+		assertNotNull(bpp.spaceURL);
+		assertNotNull(bpp.projectURL);
+		assertNotNull(bpp.getProject());
+		assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
+		assertEquals("https://demo.backlog.jp/projects/FOO", bpp.projectURL);
+		assertEquals("FOO", bpp.getProject());
 	}
 
 	@Test
 	public final void testSpaceURL() {
-        bpp = new BacklogProjectProperty("https://demo.backlog.jp/");
-        assertNotNull(bpp.spaceURL);
-        assertNull(bpp.projectURL);
-        assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
+		bpp = new BacklogProjectProperty("https://demo.backlog.jp/");
+		assertNotNull(bpp.spaceURL);
+		assertNull(bpp.projectURL);
+		assertNull(bpp.getProject());
+		assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
 
-        bpp = new BacklogProjectProperty("https://demo.backlog.jp");
-        assertNotNull(bpp.spaceURL);
-        assertNull(bpp.projectURL);
-        assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
+		bpp = new BacklogProjectProperty("https://demo.backlog.jp");
+		assertNotNull(bpp.spaceURL);
+		assertNull(bpp.projectURL);
+		assertNull(bpp.getProject());
+		assertEquals("https://demo.backlog.jp/", bpp.spaceURL);
 	}
 }
