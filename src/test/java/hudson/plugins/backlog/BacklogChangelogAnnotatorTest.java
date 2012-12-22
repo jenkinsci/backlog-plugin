22a9525963bc7c6cb38f4b86e77b579c82a0989f
emanon001 <emanon001@gmail.com>
2012-12-23 05:26:44 +0900

    Fix the issue key pattern (when "_" is included in the issue key)

--- src/test/java/hudson/plugins/backlog/BacklogChangelogAnnotatorTest.java
+++ src/test/java/hudson/plugins/backlog/BacklogChangelogAnnotatorTest.java
@@ -20,37 +20,35 @@ public class BacklogChangelogAnnotatorTest {
 	@Test
 	public final void testAnnotate() {
 		{
-			String src = "[[BLG-123]]について、対応しました。";
+			String src = "[[BLG_2-123]]について、対応しました。";
 			MarkupText text = new MarkupText(src);
 			annotator.annotate("https://backlog.backlog.jp/", text);
 			Assert.assertEquals(
-					"<a href=\"https://backlog.backlog.jp/view/BLG-123\">[[BLG-123]]</a>について、対応しました。",
-					text.toString(false));
-		}
-
-		{
-			String src = "BLG-123について、対応しました。";
-			MarkupText text = new MarkupText(src);
-			annotator.annotate("https://backlog.backlog.jp/", text);
-			Assert.assertEquals(
-					"<a href=\"https://backlog.backlog.jp/view/BLG-123\">BLG-123</a>について、対応しました。",
-					text.toString(false));
-		}
-
-		{
-			String src = "IEの場合に表示がおかしかったのを修正（BLG-1384）";
-			MarkupText text = new MarkupText(src);
-			annotator.annotate("https://backlog.backlog.jp/", text);
-			Assert.assertEquals(
-					"IEの場合に表示がおかしかったのを修正（<a href=\"https://backlog.backlog.jp/view/BLG-1384\">BLG-1384</a>）",
+					"<a href=\"https://backlog.backlog.jp/view/BLG_2-123\">[[BLG_2-123]]</a>について、対応しました。",
 					text.toString(false));
 		}
 		{
-			String src = "2009-05-15 IEの場合に表示がおかしかったのを修正（BLG-1384）";
+			String src = "BLG_2-123について、対応しました。";
 			MarkupText text = new MarkupText(src);
 			annotator.annotate("https://backlog.backlog.jp/", text);
 			Assert.assertEquals(
-					"2009-05-15 IEの場合に表示がおかしかったのを修正（<a href=\"https://backlog.backlog.jp/view/BLG-1384\">BLG-1384</a>）",
+					"<a href=\"https://backlog.backlog.jp/view/BLG_2-123\">BLG_2-123</a>について、対応しました。",
+					text.toString(false));
+		}
+		{
+			String src = "IEの場合に表示がおかしかったのを修正（BLG_2-1384）";
+			MarkupText text = new MarkupText(src);
+			annotator.annotate("https://backlog.backlog.jp/", text);
+			Assert.assertEquals(
+					"IEの場合に表示がおかしかったのを修正（<a href=\"https://backlog.backlog.jp/view/BLG_2-1384\">BLG_2-1384</a>）",
+					text.toString(false));
+		}
+		{
+			String src = "2009-05-15 IEの場合に表示がおかしかったのを修正（BLG_2-1384）";
+			MarkupText text = new MarkupText(src);
+			annotator.annotate("https://backlog.backlog.jp/", text);
+			Assert.assertEquals(
+					"2009-05-15 IEの場合に表示がおかしかったのを修正（<a href=\"https://backlog.backlog.jp/view/BLG_2-1384\">BLG_2-1384</a>）",
 					text.toString(false));
 		}
 	}
@@ -58,7 +56,7 @@ public class BacklogChangelogAnnotatorTest {
 	@Test
 	public final void testIssuePattern() {
 		{
-			String src = "[[BLG-123]]について、対応しました。";
+			String src = "[[BLG_2-123]]について、対応しました。";
 			Matcher m = BacklogChangelogAnnotator.ISSUE_KEY_PATTERN
 					.matcher(src);
 			while (m.find()) {
@@ -70,7 +68,7 @@ public class BacklogChangelogAnnotatorTest {
 			}
 		}
 		{
-			String src = "BLG-123について、対応しました。";
+			String src = "BLG_2-123について、対応しました。";
 			Matcher m = BacklogChangelogAnnotator.ISSUE_KEY_PATTERN
 					.matcher(src);
 			while (m.find()) {
