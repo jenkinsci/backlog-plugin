03b213c17386c07e450a5dfdfbfce55292510e30
nakamura <nakamura@nulab.co.jp>
2015-06-21 12:10:53 +0900

    Add ApiKey field to execute Backlog API v2

--- src/test/java/hudson/plugins/backlog/BacklogLinkActionTest.java
+++ src/test/java/hudson/plugins/backlog/BacklogLinkActionTest.java
@@ -1,14 +1,20 @@
 package hudson.plugins.backlog;
 
+import com.nulabinc.backlog4j.*;
+import com.nulabinc.backlog4j.api.option.PullRequestQueryParams;
+import com.nulabinc.backlog4j.conf.BacklogConfigure;
+import com.nulabinc.backlog4j.conf.BacklogJpConfigure;
 import org.junit.Test;
 import org.jvnet.hudson.test.HudsonTestCase;
+
+import java.util.Arrays;
 
 public class BacklogLinkActionTest extends HudsonTestCase {
 
 	@Test
 	public final void testGetUrlName() {
 		BacklogProjectProperty prop = new BacklogProjectProperty(
-				"https://demo.backlog.jp", "test", "test");
+				"https://demo.backlog.jp", "test", "test", "apiKey");
 		BacklogLinkAction action = new BacklogLinkAction(prop);
 		assertEquals("https://demo.backlog.jp/", action.getUrlName());
 	}
@@ -16,7 +22,7 @@ public class BacklogLinkActionTest extends HudsonTestCase {
 	@Test
 	public final void testGetUrlNameProject() {
 		BacklogProjectProperty prop = new BacklogProjectProperty(
-				"https://demo.backlog.jp/projects/DORA", "test", "test");
+				"https://demo.backlog.jp/projects/DORA", "test", "test", "apiKey");
 		BacklogLinkAction action = new BacklogLinkAction(prop);
 		assertEquals("https://demo.backlog.jp/projects/DORA",
 				action.getUrlName());
